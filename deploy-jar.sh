c44044ea86e765bd79a0306e10b1747e52d81bb9
xinj.xue <woxuexxj@126.com>
2018-06-19 06:52:43 +0800

    添加部署脚本

---
+++ deploy-jar.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#编译+部署upload-demo
+
+#需要配置如下参数
+# 项目路径，在Execute Shell中配置项目路径，pwd即可获得该项目路径
+# export PROJ_PATH=这个jenkins任务在部署机器上的路径
+
+# 输入你的环境上tomcat的全路径
+# TOMCAT_APP_PATH=tomcat在部署机器上的路径
+
+
+cd $PROJ_PATH/upload-demo
+mvn clean package -DskipTests
+
+# 启动应用
+cd $PROJ_PATH
+mkdir logs
+
+cd upload-demo/target
+
+# 后台启动应用，并将日志输出到指定文件
+nohup java -jar upload-demo-0.0.1-SNAPSHOT.jar > $PROJ_PATH/logs/upload-demo.log &
