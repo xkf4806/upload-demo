a5b2ef6af09277ea957ff0ddfb352b548d8376d0
xinj.xue <woxuexxj@126.com>
2018-06-19 07:21:30 +0800

    重命名title

--- deploy-jar.sh
+++ deploy-jar.sh
@@ -12,10 +12,24 @@
 cd $PROJ_PATH/upload-demo
 mvn clean package -DskipTests
 
-# 启动应用
+# 创建文件夹
+if [ ! -d "logs" ];then
 mkdir logs
+else
+echo "logs日志文件夹已经存在"
+fi
 
 cd target
 
-# 后台启动应用，并将日志输出到指定文件
+# 查询应用的pid
+pid=`ps -ef | grep upload-demo | grep java | awk '{print $2}'`
+
+echo "upload-demo进程号：$pid"
+if ["$pid" = ""]
+then
+    echo "未检测到正在执行的upload-demo应用"
+else
+    kill -9 $pid
+fi
+# 重新后台启动应用，并将日志输出到指定文件
 nohup java -jar upload-demo-0.0.1-SNAPSHOT.jar > $PROJ_PATH/upload-demo/logs/upload-demo.log &
